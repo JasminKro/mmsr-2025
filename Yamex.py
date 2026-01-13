3df452df29088c1b1f2c44ff52baa40b19c45e23
Andrea Zoitl <strickarium@gmail.com>
2026-01-13 14:57:17 +0100

    UI: code now works with new flet version
    All necessary changes to the new methods/syntax have been implemented. There is no longer an ElevatedButton; it has been replaced by a normal button

--- Yamex.py
+++ Yamex.py
@@ -2,7 +2,6 @@ import flet as ft
 import pandas as pd
 
 from enum import Enum
-
 from strategies import RandomStrategy, UnimodalStrategy
 from unimodal import UnimodalRetrievalSystem, Evaluator
 
@@ -16,6 +15,12 @@ class RetrievalAlgorithms(str, Enum):
 
 DATA_ROOT = "./data"
 id_information_df = pd.read_csv(f"{DATA_ROOT}/id_information_mmsr.tsv", sep="\t")
+id_url_df = pd.read_csv(f"{DATA_ROOT}/id_url_mmsr.tsv", sep="\t")
+
+
+# Lookup dictionary
+# This maps 'id' -> {'song': ..., 'artist': ..., 'album_name': ..., 'url': ...}
+song_lookup_dict = id_information_df.merge(id_url_df, on="id", how="left").set_index("id").to_dict("index")
 
 evaluator = Evaluator(DATA_ROOT)
 unimodal_rs = UnimodalRetrievalSystem(DATA_ROOT, evaluator)
@@ -24,17 +29,18 @@ current_algorithm = RetrievalAlgorithms.RANDOM
 retrieved_results = []
 
 def main(page: ft.Page):
+    page.scroll = ft.ScrollMode.AUTO
     page.padding = 20
     page.bgcolor = ft.Colors.DEEP_PURPLE_900
     page.theme = ft.Theme(
         color_scheme=ft.ColorScheme(
             primary=ft.Colors.DEEP_PURPLE_300,
             secondary=ft.Colors.DEEP_PURPLE_200,
-            background=ft.Colors.DEEP_PURPLE_900,
+            surface_container=ft.Colors.DEEP_PURPLE_900,
             surface=ft.Colors.DEEP_PURPLE_800,
             on_primary=ft.Colors.WHITE,
             on_secondary=ft.Colors.WHITE,
-            on_background=ft.Colors.WHITE,
+            on_surface_variant=ft.Colors.WHITE,
             on_surface=ft.Colors.WHITE
         ),
         text_theme=ft.TextTheme(
@@ -72,8 +78,9 @@ def main(page: ft.Page):
 
     slider_label = ft.Text(f"Number of results: {current_slider_value}", color=ft.Colors.WHITE)
 
-    def handle_slider(e: ft.ControlEvent):
+    def handle_slider(e: ft.ControlEvent):#
         global current_slider_value
+#        if isinstance(e.control, ft.Slider):
         current_slider_value = int(e.control.value)
         slider_label.value = f" Number of results: {current_slider_value}"
         page.update()
@@ -86,15 +93,16 @@ def main(page: ft.Page):
 
     def create_search_field(
         hint_text: str = "Search for a song title, an artist or an album",
-        expand=False,
         on_submit_callback=None
     ):
         return ft. TextField(
             hint_text=hint_text,
+            hint_style=ft.TextStyle(color=ft.Colors.DEEP_PURPLE_800),
             bgcolor=ft.Colors.DEEP_PURPLE_50,
             border_radius=20,
-            prefix_icon=ft.Icons.SEARCH_ROUNDED,
-            expand=False,
+            color=ft.Colors.BLACK,
+            prefix_icon=ft.Icon(ft.Icons.SEARCH_ROUNDED, color=ft.Colors.DEEP_PURPLE_800),
+            expand=True,
             on_submit = lambda e: on_submit_callback(e.control.value)
                 if on_submit_callback else None
         )
@@ -111,7 +119,7 @@ def main(page: ft.Page):
             return
 
         # Select the strategy based on the dropdown value
-        selected_algo_key = dropdown_algorithm.value
+        selected_algo_key = RetrievalAlgorithms(dropdown_algorithm.value)
         strategy = strategies.get(selected_algo_key)
 
         if not strategy:
@@ -119,15 +127,16 @@ def main(page: ft.Page):
             page.update()
             return
 
-        # EXECUTE SEARCH
+        # execute search
         ids, scores = strategy.search(query_id, current_slider_value)
 
-        # UPDATE UI
+        # update UI
         retrieved_results.clear()
         for i, (ret_id, score) in enumerate(zip(ids, scores)):
-            res_row = id_information_df[id_information_df["id"] == ret_id]
-            if res_row.empty: continue
-            res = res_row.iloc[0]
+            res = song_lookup_dict.get(ret_id)
+
+            if not res:
+                continue
 
             song_info = {
                 "index": i + 1,
@@ -135,14 +144,15 @@ def main(page: ft.Page):
                 "score": score,
                 "song": res["song"],
                 "artist": res["artist"],
-                "album_name": res["album_name"]
+                "album_name": res["album_name"],
+                "url": res["url"]
             }
             retrieved_results.append(song_info)
 
             result_songs.controls.append(
                 ft.ListTile(
                     leading=ft.Text(f"[{song_info['index']}]", color="white70", size=14),
-                    title=ft.Text(f"{song_info['song']}", color="white", weight="bold"),
+                    title=ft.Text(f"{song_info['song']}", color="white", weight=ft.FontWeight.BOLD),
                     subtitle=ft.Text(f"{song_info['artist']}, {song_info['album_name']} | Score: {score:.3f}", color="white70"),
                     on_click=lambda e, s=song_info: on_song_click(s)
                 )
@@ -167,17 +177,28 @@ def main(page: ft.Page):
 
 
     def on_song_click(song_data):
+        video_url = song_data.get("url", "")
+
+        # Convert standard YouTube link to Embed link
+        embed_url = ""
+        if "youtube.com" in video_url:
+            video_id = video_url.split("v=")[-1].split("&")[0] if "v=" in video_url else video_url.split("/")[-1]
+            embed_url = f"https://www.youtube.com/embed/{video_id}"
+
         # Update the details container with info from the clicked song
         result_container.content = ft.Column([
-            ft.Text(f"Title: {song_data['song']}", size=20, weight="bold"),
+            ft.Text(f"Title: {song_data['song']}", size=20, weight=ft.FontWeight.BOLD),
             ft.Text(f"Artist: {song_data['artist']}"),
             ft.Text(f"Album: {song_data['album_name']}"),
             ft.Text(f"Score: {song_data['score']:.4f}"),
             ft.Text(f"ID: {song_data['id']}", size=12, color="grey"),
-        ])
+            ft.Divider(height=20, color="transparent"),
+
+            # The Video Player
+            # TODO
+                ft.Text(f"ID: {song_data['id']}", size=12, color="grey"),
+            ], scroll=ft.ScrollMode.AUTO)
         page.update()
-
-
     search_field = create_search_field(
     )
 
@@ -210,7 +231,7 @@ def main(page: ft.Page):
             ft.dropdown.Option(RetrievalAlgorithms.LATE_FUSION.value, "Multimodal - Late fusion"),
             ft.dropdown.Option(RetrievalAlgorithms.NEUTRAL_NETWORK.value, "Neural-Network based")
         ],
-        on_change=handle_dropdown_menu,
+        on_select=handle_dropdown_menu,
         border_radius=20,
         bgcolor=ft.Colors.DEEP_PURPLE_700,
         border_color=ft.Colors.DEEP_PURPLE_200,
@@ -220,9 +241,12 @@ def main(page: ft.Page):
         expand=True
     )
 
-    search_button = ft.ElevatedButton(
-        text="Search Now",
-        icon=ft.Icons.SEARCH,
+    search_button = ft.Button(
+        content=ft.Row(
+            [ft.Icon(ft.Icons.SEARCH), ft.Text("Search Now")],
+            alignment=ft.MainAxisAlignment.CENTER,
+            tight=True,
+        ),
         on_click=handle_search_now,
         color=ft.Colors.WHITE,
         bgcolor=ft.Colors.DEEP_PURPLE_800,
@@ -232,27 +256,27 @@ def main(page: ft.Page):
                 color=ft.Colors.DEEP_PURPLE_200,
                 width=1
             ),
-            padding=ft.padding.symmetric(horizontal=20, vertical=20),
+            # Padding in ButtonStyle also uses the Padding class
+            padding=ft.Padding(20, 20, 20, 20),
         ),
         col={"xs": 8, "sm": 4, "md": 3},
     )
-
 
     control_row = ft.ResponsiveRow(
         controls=[
             ft.Container(
                 content=search_field,
-                alignment=ft.alignment.center_right,
+                alignment=ft.alignment.Alignment.CENTER_RIGHT,
                 col={"xs": 12, "md": 6},
             ),
             ft.Container(
                 content=dropdown_algorithm,
-                alignment=ft.alignment.center_left,
+                alignment=ft.alignment.Alignment.CENTER_LEFT,
                 col={"xs": 12, "md": 2},
             ),
             ft.Container(
                 content=slider_group,
-                alignment=ft.alignment.center_left,
+                alignment=ft.alignment.Alignment.CENTER_LEFT,
                 col={"xs": 12, "md": 4},
             )
         ],
@@ -260,25 +284,28 @@ def main(page: ft.Page):
         spacing=20
     )
 
-    result_songs = ft.Column(scroll="auto", expand=True)
+    result_songs = ft.Column(
+        scroll=ft.ScrollMode.ALWAYS,
+        expand=True,
+        height=500)
 
     intermediate_results_container = ft.Container(
         content=result_songs,
         padding=15,
-        border=ft.border.all(1, ft.Colors.DEEP_PURPLE_200),
+        border=ft.Border.all(1, ft.Colors.DEEP_PURPLE_200),
         border_radius=20,
-        expand=True,
-        alignment=ft.alignment.top_left,
+#        expand=True,
+        alignment=ft.Alignment.TOP_LEFT,
         col={"xs": 12, "md": 6}  # xs = small monitor: full width, md = medium = 6 of 12 colums width
     )
 
     result_container = ft.Container(
         content=ft.Text("Details", color=ft.Colors.WHITE),
         padding=15,
-        border=ft.border.all(1, ft.Colors.DEEP_PURPLE_200),
+        border=ft.Border.all(1, ft.Colors.DEEP_PURPLE_200),
         border_radius=20,
         expand=True,
-        alignment=ft.alignment.top_left,
+        alignment=ft.Alignment.TOP_LEFT,
         col={"xs": 12, "md": 6}
     )
 
@@ -300,10 +327,13 @@ def main(page: ft.Page):
                 ft.Text("Top results:"),
                 result_row
             ],
-            horizontal_alignment="center",
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=25,
             expand=True
         )
     )
 
-ft.app(target=main, view=ft.WEB_BROWSER)
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)  # to run Yamex in PyCharm
+# ft.app(target=main) # to run Yamex in terminal go saved directory and use command: flet run --web Yamex.py
+# ft.run(main) # alternative for opening a window
